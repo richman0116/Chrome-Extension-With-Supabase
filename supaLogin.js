416b314dcce0cbbd504e022a90df703692fa6fd2
richman0116 <maria8partee@gmail.com>
2023-10-16 05:20:26 -0700

    popup-redirect

--- supaLogin.js
+++ supaLogin.js
@@ -5,45 +5,69 @@ const { createClient } = supabase;
 supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 console.log(supabase)
+getSession()
+
+async function SignIn(){
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'google'
+      })
+
+    console.log('data.url', data?.url)
+    console.log('error', error)
+}
+
+async function SignOut(){
+    const { error } = await supabase.auth.signOut()
+
+    if (!error){
+        //window.location.reload();
+        window.close()
+    }
+}
+
+async function getSession() {
+    const { data, error } = await supabase.auth.getSession();
+  
+    console.log('data', data);
+    console.log('error', error);
+  
+    if (data?.session) {
+      const profileElement = document.getElementById('profile');
+
+      if (profileElement) {
+        profileElement.innerHTML = 
+        `
+          <img src='${data?.session?.user?.user_metadata?.avatar_url}'>
+          <p>You are logged in as ${data?.session?.user?.user_metadata?.name}</p>
+          <button id="signout-button">Log Out</button>
+        `;
+      }
+
+    }
+  }
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    const loginButton = document.getElementById('login-button');
+    const loginButton = document?.getElementById('login-button');
+    const siginButton = document?.getElementById('signin-button');
+    const sigoutButton = document?.getElementById('signout-button');
 
-    loginButton.addEventListener("click", function (e) {
-        console.log('clicked')
+    loginButton?.addEventListener("click", function (e) {
+        chrome.windows.create({
+            url: './redirect.html',
+            width: 300,
+            height: 600,
+            focused: true,
+        });
 
-        const manifest = chrome.runtime.getManifest()
-
-        const url = new URL('https://accounts.google.com/o/oauth2/auth')
-
-        url.searchParams.set('client_id', manifest.oauth2.client_id)
-        url.searchParams.set('response_type', 'id_token')
-        url.searchParams.set('access_type', 'offline')
-        url.searchParams.set('redirect_uri', `https://${chrome.runtime.id}.chromiumapp.org`)
-        url.searchParams.set('scope', manifest.oauth2.scopes.join(' '))
-
-        console.log(url.href)
-
-        chrome.identity.launchWebAuthFlow(
-        {
-            url: url.href,
-            interactive: true,
-        },
-        async (redirectedTo) => {
-            if (chrome.runtime.lastError) {
-            // auth was not successful
-            } else {
-            // auth was successful, extract the ID token from the redirectedTo URL
-            const url = new URL(redirectedTo)
-            const params = new URLSearchParams(url.hash)
-
-            const { data, error } = await supabase.auth.signInWithIdToken({
-                provider: 'google',
-                token: params.get('id_token'),
-            })
-            }
-        }
-        )
-
+        window.close(); // Close the extension popup
     });
-});
+
+    siginButton?.addEventListener("click", function (e) {
+        SignIn();
+    });
+
+    sigoutButton?.addEventListener("click", function (e) {
+        console.log('click')
+        SignOut();
+    });
+})
